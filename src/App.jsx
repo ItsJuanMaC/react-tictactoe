59bec6797ddc0918986ee4f92ee37d808b2c5366
juanm <juanmacastro1000@gmail.com>
2026-03-17 17:58:00 -0500

    Teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+function Cuadro() {
+  return (
+    <button className="cuadro">X</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <p>Welcome to my React application.</p>
+      <p>Welcome to my React application.</p> 
+      <Cuadro />
     </div>
   );
 } 
